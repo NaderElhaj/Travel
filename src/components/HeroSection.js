0a263dca21d3bc7a244073195a33524fa203b37b
Nader elhaj <naderelhaj3@gmail.com>
2021-02-18 02:51:37 +0100

    Structuring Again And Styling Hero Section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import '../App.css'
 import { Button } from './Button'
+import './HeroSection.css'
 
 const HeroSection = () => {
     return (
         <div className='hero-container'>
-           <video src="/video/video-2.mp4"  autoPlay loop muted/>
+           <video src="../video/video-2.mp4"  autoPlay loop muted/>
            <h1>ADVENTURE AWAITW</h1>
            <p>What are you waiting for ? </p>
            <div className="hero-btns">
@@ -12,6 +14,12 @@ const HeroSection = () => {
                className="btns" 
                buttonStyle="btn--outline"
                buttonSize="btn--large">GetStarted</Button>
+               <Button 
+               className="btns" 
+               buttonStyle="btn--primary"
+               buttonSize="btn--large">
+               Watch Trailer <i className="far fa-play-circle"></i>
+               </Button>
            </div>
 
         </div>
