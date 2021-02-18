74973de89c468e58ff4391dc870ae0db06d8da30
Nader elhaj <naderelhaj3@gmail.com>
2021-02-18 02:31:51 +0100

    adding videos

---
+++ src/components/HeroSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <div className='hero-container'>
+           <video src="/video/video-2.mp4"></video> 
+        </div>
+    )
+}
+
+export default HeroSection
