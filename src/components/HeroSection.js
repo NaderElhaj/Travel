61c9989217f78c9fed27337ccc1acefdfb67b1e6
Nader elhaj <naderelhaj3@gmail.com>
2021-02-18 02:42:53 +0100

    Hero Structure

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Button } from './Button'
 
 const HeroSection = () => {
     return (
         <div className='hero-container'>
-           <video src="/video/video-2.mp4"></video> 
+           <video src="/video/video-2.mp4"  autoPlay loop muted/>
+           <h1>ADVENTURE AWAITW</h1>
+           <p>What are you waiting for ? </p>
+           <div className="hero-btns">
+               <Button 
+               className="btns" 
+               buttonStyle="btn--outline"
+               buttonSize="btn--large">GetStarted</Button>
+           </div>
+
         </div>
     )
 }
