3da338aaab46e7b4752dfcc1ed60c420506b48ba
Nader elhaj <naderelhaj3@gmail.com>
2021-02-20 02:23:23 +0100

    Structuring Card Section$

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 import { Route, Switch } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import HeroSection from "./components/HeroSection";
-import Asba from "./components/Asba";
+import Cards from "./components/Cards";
+import Home from "./components/pages/Home";
+
 
 
 function App() {
@@ -11,10 +13,9 @@ function App() {
     <>
      <Router>
        <Switch>
-       <Route exact path="/"  component={HeroSection} />
-       <Route path="/sign-up" component={Asba} />
-
+       <Route exact path="/"  component={Home} />
        </Switch>
+
      </Router>
       
     </>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import CardItem from './CardItem'
+import React from 'react';
+import './Cards.css';
+import CardItem from './CardItem';
 
-const Cards = () => {
-    return (
-        <div className="cards">
-            <h1>Check Out These Epic Destinations</h1>
-            <div className="cards__container">
-                <div className="cards__wrapper">
-                    <ul className="cards__items">
-                        <CardItem />
-                    </ul>
-                </div>
-            </div>
+function Cards() {
+  return (
+    <div className='cards'>
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+                <CardItem />
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Cards
+export default Cards;
